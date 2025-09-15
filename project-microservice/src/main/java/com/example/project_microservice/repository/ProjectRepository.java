f60b9c8e9b3dd029128879948806e3f2d2d0e54b
Melokawka <84191586+Melokawka@users.noreply.github.com>
2025-09-16 01:34:04 +0200

    Delete project-microservice directory

--- project-microservice/src/main/java/com/example/project_microservice/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project_microservice.repository;
-
-import com.example.project_microservice.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
--- project-microservice/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO projects (name, description, start_date, end_date, completion)
-VALUES
-    ('AI Research', 'Exploring ML algorithms', '2025-01-01', '2025-06-01', 20),
-    ('E-Commerce Platform', 'Building shopping site', '2025-02-15', '2025-12-31', 45),
-    ('Healthcare App', 'Mobile app for patient monitoring', '2025-03-01', '2025-09-30', 10);
